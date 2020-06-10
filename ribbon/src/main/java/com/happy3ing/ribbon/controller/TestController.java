9ca449b12e2c846ca40879aaa2a9aa13d52d5516
代蒙蒙 <mengmeng.dai@pactera.com>
2020-06-10 23:09:40 +0800

    config test

---
+++ ribbon/src/main/java/com/happy3ing/ribbon/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.happy3ing.ribbon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Author: Dai Mengmeng
+ * @Description:
+ * @Date: 2020/6/10 23:00
+ */
+@RestController
+@RequestMapping("/ribbon")
+public class TestController {
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/test")
+    public String test(){
+        return restTemplate.getForObject("http://common/common/test",String.class);
+    }
+}
